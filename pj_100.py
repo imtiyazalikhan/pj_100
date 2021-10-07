5adc3c05d72f81fa21933b1524cd566ebea03649
imtiyazalikhan <65025085+imtiyazalikhan@users.noreply.github.com>
2021-10-07 18:59:29 +0530

    Add files via upload

---
+++ pj_100.py
@@ -0,0 +1,11 @@
+class Atm:
+    def config(self):
+        print("number : 123456987")
+        print("pin : 123456")
+        print("cash with drawl : 10000000")
+        print("balance : 12000000o")
+atm1 = Atm()
+
+
+Atm.config(atm1)
+
